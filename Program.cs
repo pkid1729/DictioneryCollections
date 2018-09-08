e7f6b8e3133a17b93b3764d419462511c879a38b
pkid1729 <30475130+pkid1729@users.noreply.github.com>
2018-09-08 01:47:21 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictioneryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string,object> dt=new Dictionary<string,object>();
+           dt.Add("C_ID",1001);
+           dt.Add("C_Name","Pranav Kumar");
+           dt.Add("Specialization","CSE");
+           dt.Add("Phone",7358163322);
+           dt.Add("EMail","pkid1729@gmail.com");
+           dt.Add("Salary",50000);
+           dt.Add("Location","Chennai");
+           foreach(string key in dt.Keys)
+           Console.WriteLine(key +":"+dt[key]);
+           Console.ReadLine();
+
+        }
+    }
+}
